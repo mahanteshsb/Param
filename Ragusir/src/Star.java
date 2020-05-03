8ddb5c0bcf86f08f0782b92b8bc46bffc0791dec
DELL <DELL@HP1>
2020-05-04 04:48:23 +0530

    # WARNING: head commit changed in the meantime
    theard commit

--- Ragusir/src/Star.java
+++ Ragusir/src/Star.java
@@ -13,7 +13,7 @@ System.out.println("enter month");
 {
  for(int j=1;j<=i;j++)
  {
-System.out.print(i);
+System.out.print("i");
 }
 
 System.out.println();
--- TestNgDemo/src/Login.java
+++ TestNgDemo/src/Login.java
@@ -1,4 +1,38 @@
+import java.util.Scanner;
 
 public class Login {
+	public static void main(String[] args) {
+		System.out.println("Enter the numbber");
+
+		Scanner ss=new Scanner(System.in);
+		int n=ss.nextInt();
+		
+		System.out.println("Enter the gender");
+		Scanner ss1=new Scanner(System.in);
+
+		String gend=ss1.nextLine();
+
+		if(n>20&&n<40)
+		{
+		     System.out.println("any");
+		}
+		else if(n>40)
+		 {
+
+		      if(gend.equals("female"))
+		      {
+					 System.out.println("urban");
+		    	  
+		      }
+		       else if(gend.equals("male"))
+		       {
+		              System.out.println("any");
+		       }
+	     }
+		 else if(n<=20){ 
+		        System.out.println("value is less than 20 i don't know what to print");
+		}
+
+	}
 
 }
